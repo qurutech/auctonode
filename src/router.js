e2dd77041f7a6fc68b42238221b73f7c681a7a8c
Kelvin Omereshone <kelvinomereshone@gmail.com>
2019-04-08 10:51:13 +0100

    removed authorize feature

--- src/router.js
+++ src/router.js
@@ -29,32 +29,37 @@ export const router =  new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/Vote.vue')
     },
-    {
-      path: '/authorize',
-      name: 'authorize',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Authorize.vue')
-    }
+    // {
+    //   path: '/authorize',
+    //   name: 'authorize',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/Authorize.vue')
+    // }
   ]
 });
 
 router.beforeEach((to, from, next) => {
   // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/authorize',];
+  // const publicPages = ['/authorize',];
+  const publicPages = ['/',];
   const authRequired = !publicPages.includes(to.path);
   const loggedIn = localStorage.getItem('isLoggedIn');
-  const authorized = localStorage.getItem('authorize'); 
-    if (to.path !== '/authorize' ) {
-      if (authorized && to.path !== '/' && !loggedIn) {
-        return next('/');
-      } else if (authorized && authRequired && !loggedIn && to.path !== '/')  {
-        return next('/');
-      } else if (!authorized  && to.path !== '/authorize') {
-        return next('/authorize');
-      }
+  // const authorized = localStorage.getItem('authorize'); 
+    // if (to.path !== '/authorize' ) {
+    //   if (authorized && to.path !== '/' && !loggedIn) {
+    //     return next('/');
+    //   } else if (authorized && authRequired && !loggedIn && to.path !== '/')  {
+    //     return next('/');
+    //   } else if (!authorized  && to.path !== '/authorize') {
+    //     return next('/authorize');
+    //   }
 
+    // }
+
+    if (authRequired && !loggedIn && to.path !== '/') {
+      return next('/');
     }
-  next();
+    next();
 });
